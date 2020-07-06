8cb2fd55f384516c29862bdf2c2a6dfbfa2a86aa
zwelisha <zwescientist@gmail.com>
2020-07-06 07:46:33 +0200

    sorting out deployment issues

--- app.py
+++ app.py
@@ -3,3 +3,8 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
