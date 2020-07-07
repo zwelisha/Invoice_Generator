70e15da47176537dd9e7de4a6f0de2fde41bdf02
zwelisha <zwescientist@gmail.com>
2020-07-07 15:05:18 +0200

    Fixing ajax API call

--- static/js/main.js
+++ static/js/main.js
@@ -18,82 +18,125 @@ $(document).ready(function() {
 
     // Binding previous and next event and form submission
     // Binding next button on first step
-    $(".open1").click(function() {
+    // $(".open1").click(function() {
         // if(validateForm.form()) {
         //     $(".frm").hide("fast");
         //     $("#step2").show("slow");
-        // }
-        $(".frm").hide("fast");
-        $("#step2").show("slow");
-    });
+    //     // }
+    //     $(".frm").hide("fast");
+    //     $("#step2").show("slow");
+    // });
 
-    // Binding next button on second step
-    $(".open2").click(function() {
-        // if(validateForm.form()) {
-        //     $(".frm").hide("fast");
-        //     $("#step3").show("slow");
-        // }
-        $(".frm").hide("fast");
-        $("#step3").show("slow");
-    });
+    // // Binding next button on second step
+    // $(".open2").click(function() {
+    //     // if(validateForm.form()) {
+    //     //     $(".frm").hide("fast");
+    //     //     $("#step3").show("slow");
+    //     // }
+    //     $(".frm").hide("fast");
+    //     $("#step3").show("slow");
+    // });
 
-     // Binding back button on second step
-    $(".back2").click(function() {
-        $(".frm").hide("fast");
-        $("#step1").show("slow");
-    });
+    //  // Binding back button on second step
+    // $(".back2").click(function() {
+    //     $(".frm").hide("fast");
+    //     $("#step1").show("slow");
+    // });
 
     // Binding back button on third step
-    $(".back3").click(function() {
-        $(".frm").hide("fast");
-        $("#step2").show("slow");
-    });
+    // $(".back3").click(function() {
+    //     $(".frm").hide("fast");
+    //     $("#step2").show("slow");
+    // });
 
     // var server = "http://localhost:5000";
-    var apiUrl = "/api/send_email"
+    
     var invoiceData = {
-        toFirstName: $('#toFirstName').val(),
-        toLastName: $('#toLastName').val(),
-        companyName: $('#companyName').val(),
-        companyAddress: $('companyAddress').val(),
-        sentDate: $('#sentDate').val(),
-        toEmail: $('#toEmail').val(),
-        fromFirstName: $('#fromFirstName').val(),
-        fromLastName: $('#fromLastName').val(),
-        fromEmail: $('#fromEmail').val(),
-        password: $('#password').val(),
-        bankName: $('#bankName').val(),
-        accNumber: $('#accNumber').val(),
-        reference: $('#reference').val(),
-        dailyRate: $('#dailyRate').val(),
-        quantity: $('#quantity').val(),
-        description: $('#description').val(),
-        startedOn: $('#started').val(),
-        completedOn: $('#completed').val(),
-    };
-
-    invoiceData['total'] = invoiceData.dailyRate * invoiceData.quantity;
-
-    $(function() {
+        // toFirstName: $('#toFirstName').val(),
+        // toLastName: $('#toLastName').val(),
+        // companyName: $('#companyName').val(),
+        // companyAddress: $('companyAddress').val(),
+        // sentDate: $('#sentDate').val(),
+        // toEmail: $('#toEmail').val(),
+        // fromFirstName: $('#fromFirstName').val(),
+        // fromLastName: $('#fromLastName').val(),
+        // fromEmail: $('#fromEmail').val(),
+        // password: $('#password').val(),
+        // bankName: $('#bankName').val(),
+        // accNumber: $('#accNumber').val(),
+        // reference: $('#reference').val(),
+        // dailyRate: $('#dailyRate').val(),
+        // quantity: $('#quantity').val(),
+        // description: $('#description').val(),
+        // startedOn: $('#started').val(),
+        // completedOn: $('#completed').val(),
         
-        $(".open3").click(function() {
-            if (validateForm.form()) {
-                // optional
-                // used delay form submission for a seccond and show a loader image
-                $("#loader").show();
-                setTimeout(function(){
-                    $("#invoiceData").html('<div class="alert alert-success"><strong>Success!</strong> Your data have successfully been sent.</div>');
-                }, 1000);
-                
-                $.ajax({
-                    type: "POST",
-                    url: apiUrl,
-                    data: JSON.stringify(invoiceData),
-                    dataType: 'json'
-                }).done(function(data){
-                    console.log(data);
-                });
-            }
-        });
+        
+        
+    };
+    // Test API call
+    $("#sendDataBtn").click(function() {
+        var apiUrl = "/api/send_email";
+        var invoiceData = {
+            toFirstName: 'Adriano',
+            toLastName: 'Iorio',
+            companyName: 'Turati',
+            companyAddress: '123 Goku St',
+            sentDate: '27-11-2020',
+            toEmail: 'zwelisha@turati.co.za',
+            fromFirstName: 'Tshepo',
+            fromLastName: 'Mohlatlole',
+            fromEmail: 'zwescientist@gmail.com',
+            password: 'testPass123',
+            bankName: 'Standard Bank',
+            accNumber: 1234557890,
+            reference: 'Turati',
+            dailyRate: 175,
+            quantity: 10,
+            description: 'Turati Internship',
+            startedOn: '10-06-2020',
+            completedOn: '20-06-2020'
+        };
+        if (validateForm.form()) {
+            // optional
+            // used delay form submission for a seccond and show a loader image
+            $("#loader").show();
+            setTimeout(function(){
+                $("#invoiceData").html('<div class="alert alert-success"><strong>Success!</strong> Your data have successfully been sent.</div>');
+            }, 1000);
+            
+            $.ajax({
+                type: "POST",
+                url: apiUrl,
+                data: JSON.stringify(invoiceData),
+                dataType: 'json'
+            }).done(function(data){
+                console.log(data);
+            });
+        }
     });
+
+    // invoiceData['total'] = invoiceData.dailyRate * invoiceData.quantity;
+    // $(function() {
+        
+    //     $(".open3").click(function() {
+    //         if (validateForm.form()) {
+    //             // optional
+    //             // used delay form submission for a seccond and show a loader image
+    //             $("#loader").show();
+    //             setTimeout(function(){
+    //                 $("#invoiceData").html('<div class="alert alert-success"><strong>Success!</strong> Your data have successfully been sent.</div>');
+    //             }, 1000);
+                
+    //             $.ajax({
+    //                 type: "POST",
+    //                 url: apiUrl,
+    //                 data: JSON.stringify(invoiceData),
+    //                 dataType: 'json'
+    //             }).done(function(data){
+    //                 console.log(data);
+    //             });
+    //         }
+    //     });
+    // });
 });
