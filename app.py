b24692b2aa7eb07379711d604e5b97ca3ec0eb92
zwelisha <zwescientist@gmail.com>
2020-07-07 14:07:18 +0200

    Test sending data

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 @app.route('/')
 def home():
@@ -7,6 +7,13 @@ def home():
 @app.route('/invoice')
 def invoice():
     return render_template('invoice.html')
+
+#  API endpoint for geeting data to send email
+@app.route('/api/send_email', methods=['POST'])
+def api_send_email():
+    response = request.get_json()
+    print(response)
+    return jsonify(response)
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
