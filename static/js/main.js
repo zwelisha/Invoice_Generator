fa331c7924bbfac98009a712fefed5d075447b05
Sjk-Lacoste <tmohlatlole@gmail.com>
2020-07-07 16:49:13 +0200

    Reamoved button onlick and validation

--- static/js/main.js
+++ static/js/main.js
@@ -1,36 +1,32 @@
 $(document).ready(function() {
-    var validateForm = $("#invoiceData").validate({
-        rules: {
-            toFirstName: {
-                required: true,
-                minlength: 4,
-                maxlength: 20
-            },
-            toLastName: {
-                required: true,
-                minlength: 4,
-                maxlength: 20
-            }
-        },
-        errorElement: "span",
-        errorClass: "help-inline",
-    });
+    // var validateForm = $("#invoiceData").validate({
+    //     rules: {
+    //         toFirstName: {
+    //             required: true,
+    //             minlength: 4,
+    //             maxlength: 20
+    //         },
+    //         toLastName: {
+    //             required: true,
+    //             minlength: 4,
+    //             maxlength: 20
+    //         }
+    //     },
+    //     errorElement: "span",
+    //     errorClass: "help-inline",
+    // });
 
     // Binding previous and next event and form submission
     // Binding next button on first step
     $(".open1").click(function() {
-        if(validateForm.form()) {
-            $(".frm").hide("fast");
-            $("#step2").show("slow");
-        }
+        $(".frm").hide("fast");
+        $("#step2").show("slow");
     });
 
     // Binding next button on second step
     $(".open2").click(function() {
-        if(validateForm.form()) {
-            $(".frm").hide("fast");
-            $("#step3").show("slow");
-        }
+        $(".frm").hide("fast");
+        $("#step3").show("slow");
     });
 
      // Binding back button on second step
@@ -45,50 +41,49 @@ $(document).ready(function() {
         $("#step2").show("slow");
     });
 
-    var apiUrl = "/api/send_email";
+    var apiUrl = "http://127.0.0.1:5000/api/send_email";
     var invoiceData = {
-        toFirstName: $('#toFirstName').val(),
-        toLastName: $('#toLastName').val(),
-        companyName: $('#companyName').val(),
-        companyAddress: $('companyAddress').val(),
-        sentDate: $('#sentDate').val(),
-        toEmail: $('#toEmail').val(),
-        fromFirstName: $('#fromFirstName').val(),
-        fromLastName: $('#fromLastName').val(),
-        fromEmail: $('#fromEmail').val(),
-        password: $('#password').val(),
-        bankName: $('#bankName').val(),
-        accNumber: $('#accNumber').val(),
-        reference: $('#reference').val(),
-        dailyRate: $('#dailyRate').val(),
-        quantity: $('#quantity').val(),
-        description: $('#description').val(),
-        startedOn: $('#started').val(),
-        completedOn: $('#completed').val(),
+        'toFirstName': $('#toFirstName').val(),
+        'toLastName': $('#toLastName').val(),
+        'companyName': $('#companyName').val(),
+        'companyAddress': $('companyAddress').val(),
+        'sentDate': $('#sentDate').val(),
+        'toEmail': $('#toEmail').val(),
+        'fromFirstName': $('#fromFirstName').val(),
+        'fromLastName': $('#fromLastName').val(),
+        'fromEmail': $('#fromEmail').val(),
+        'password': $('#password').val(),
+        'bankName': $('#bankName').val(),
+        'accNumber': $('#accNumber').val(),
+        'reference': $('#reference').val(),
+        'dailyRate': $('#dailyRate').val(),
+        'quantity': $('#quantity').val(),
+        'description': $('#description').val(),
+        'startedOn': $('#started').val(),
+        'completedOn': $('#completed').val(),
     };
 
-    invoiceData['total'] = invoiceData.dailyRate * invoiceData.quantity;
+    // invoiceData['total'] = invoiceData.dailyRate * invoiceData.quantity;
 
-    $(function() {
+    $('form').submit(function(event) {
+        // optional
+        // used delay form submission for a seccond and show a loader image
+        $("#loader").show();
+        setTimeout(function(){
+            $("#invoiceData").html('<div class="alert alert-success"><strong>Success!</strong> Your data have successfully been sent.</div>');
+        }, 1000);
         
-        $(".open3").click(function() {
-            if (validateForm.form()) {
-                // optional
-                // used delay form submission for a seccond and show a loader image
-                $("#loader").show();
-                setTimeout(function(){
-                    $("#invoiceData").html('<div class="alert alert-success"><strong>Success!</strong> Your data have successfully been sent.</div>');
-                }, 1000);
-                
-                $.ajax({
-                    type: "POST",
-                    url: server,
-                    data: JSON.stringify(invoiceData),
-                    dataType: 'json'
-                }).done(function(data){
-                    console.log(data);
-                });
-            }
+        // Process the form
+        $.ajax({
+            type: "POST",
+            url: apiUrl,
+            data: invoiceData,
+            dataType: 'json',
+            encode: true
+        }).done(function(data){
+            console.log(data);
         });
+
+        event.preventDefault();
     });
 });
